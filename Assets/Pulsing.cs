b0f00d56686bd63da5d6b0bc32d0d7cf6285ef2b
Robinerd <robinerd@gmail.com>
2017-01-21 11:00:23 +0100

    Added temporary music. Added pulse scripts and usage examples in scene.

---
+++ Assets/Pulsing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulsing : MonoBehaviour {
+
+    [Range(0.0f, 1.0f)]
+    public float phaseOffset = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Here we'll handle fading of instruments based on in-game progress.
+        float timeFactor = (Music.instance.baseMusic.time - phaseOffset) * Mathf.PI * 2 * (Music.BPM / 60.0f);
+        float pulse = Mathf.Sin(timeFactor);
+        pulse = pulse * 0.5f + 0.5f; //Transform from -1..1 to 0..1
+        UpdatePulsing(pulse);
+    }
+
+    /// Called every update with "pulse" following a sine wave between [0 .. 1], synchronized with music.
+    /// Subclasses will use "pulse" in different ways, for example scale or transparency.
+    protected virtual void UpdatePulsing(float pulse)
+    {
+    }
+}
