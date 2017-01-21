39750036ff2e914edccae2a158ae0113560051cb
Niklas <hallinniklas@gmail.com>
2017-01-21 15:07:22 +0100

    graphics scene

---
+++ Assets/textureScrolling.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class textureScrolling : MonoBehaviour {
+
+	private Renderer rend;
+	public Vector2 scrolling;
+
+	void Start()
+	{
+		rend = GetComponentInChildren<Renderer>();
+	}
+	
+	void Update () {
+		rend.material.SetTextureOffset("_MainTex", new Vector2(scrolling.x * Time.time, scrolling.y * Time.time));
+	}
+}
