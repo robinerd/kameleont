cf9b66956c2558d08c252bd8fe825b135cc66373
Robinerd <robinerd@gmail.com>
2017-01-22 12:50:56 +0100

    Addded pulse ground script to control the shader.

---
+++ Assets/PulsingGround.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulsingGround : Pulsing {
+
+    public float minGrid = 0.9f;
+    public float maxGrid = 0.9f;
+    public float min1 = 0.0f;
+    public float max1 = 1.0f;
+    public float min2 = 0.0f;
+    public float max2 = 1.0f;
+
+    Material material;
+
+	// Use this for initialization
+	void Start () {
+        material = GetComponent<MeshRenderer>().material;
+        material.SetFloat("_Blend", 1.0f);
+    }
+
+    // Update is called once per frame
+    protected override void UpdatePulsing(float pulse)
+    {
+        material.SetFloat("_Square1", Mathf.Lerp(min1, max1, pulse));
+        material.SetFloat("_Square2", Mathf.Lerp(max2, min2, pulse)); //Inverted order
+        material.SetFloat("_Grid", Mathf.Lerp(min1, max1, pulse));
+    }
+}
