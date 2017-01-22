2e6464afbf6302db1573299575c9ce79ea9abf96
Robinerd <robinerd@gmail.com>
2017-01-22 13:39:48 +0100

    Pulsating stones!!!!!!!

---
+++ Assets/PulsingStone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulsingStone : Pulsing
+{
+
+    public float minGrid = 0.9f;
+    public float maxGrid = 0.9f;
+    public float min = 0.0f;
+    public float max = 1.0f;
+
+    Material material;
+
+    // Use this for initialization
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+        material.SetFloat("_Blend", 1.0f);
+    }
+
+    // Update is called once per frame
+    protected override void UpdatePulsing(float pulse)
+    {
+        material.SetFloat("_Effect", Mathf.Lerp(min, max, pulse));
+    }
+}
